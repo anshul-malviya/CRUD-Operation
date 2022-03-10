0eedc962bec878a17655a7862d764efde7cf6f64
anshul malviya <malviya21anshul@gmail.com>
2022-03-10 15:59:39 +0530

    Update Edit.jsx

--- src/Components/Edit.jsx
+++ src/Components/Edit.jsx
@@ -37,7 +37,7 @@ const Edit = () => {
     }
     useEffect(() => {
         loadUserData();
-    }, []) 
+    }) 
 
     const loadUserData = async() => {
        const response =  await getUpdate(id);
